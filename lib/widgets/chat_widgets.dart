b78c70b447d326e7c75a7e3d4e4d6de9bfc20ec8
ssemart <ssemakula169@gmail.com>
2022-10-21 22:28:04 +0300

    Widgets

---
+++ lib/widgets/chat_widgets.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:vehicle_match/constants/color_pallete.dart';
+
+Widget errorContainer() {
+  return Container(
+    clipBehavior: Clip.hardEdge,
+    child: Image.asset(
+      'assets/images/img_not_available.jpeg',
+      height: 200,
+      width: 200,
+    ),
+  );
+}
+
+Widget chatImage({required String imageSrc, required Function onTap}) {
+  return OutlinedButton(
+    onPressed: onTap(),
+    child: Image.network(
+      imageSrc,
+      width: 200,
+      height: 200,
+      fit: BoxFit.cover,
+      loadingBuilder:
+          (BuildContext ctx, Widget child, ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) return child;
+        return Container(
+          decoration: BoxDecoration(
+            color: greyColor2,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          width: 200,
+          height: 200,
+          child: Center(
+            child: CircularProgressIndicator(
+              color: burgundy,
+              value: loadingProgress.expectedTotalBytes != null &&
+                      loadingProgress.expectedTotalBytes != null
+                  ? loadingProgress.cumulativeBytesLoaded /
+                      loadingProgress.expectedTotalBytes!
+                  : null,
+            ),
+          ),
+        );
+      },
+      errorBuilder: (context, object, stackTrace) => errorContainer(),
+    ),
+  );
+}
+
+Widget messageBubble(
+    {required String chatContent,
+    required EdgeInsetsGeometry? margin,
+    Color? color,
+    Color? textColor}) {
+  return Container(
+    padding: const EdgeInsets.all(10),
+    margin: margin,
+    width: 200,
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Text(
+      chatContent,
+      style: TextStyle(fontSize: 16, color: textColor),
+    ),
+  );
+}
