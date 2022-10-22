d17a4a1a747227d1ec61ca8c7e40b4962ecc8a36
mafdaniels <mafabidaniel1@gmail.com>
2022-10-22 14:03:26 +0300

    vehicle match  mobile app

---
+++ lib/constants/color_pallete.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+const Color background =
+    Color(0xfffafaff); //background for the login & register screens
+const Color introback =
+    Color(0xff9868d3); //background for the introduction screen
+const Color homeback = Color(0xfffcfcff); // background color for the homescreen
+const Color drawerback = Color(0xffc38cfc); //background for the drawer
+const Color textcolor = Colors.black;
+
+const kPrimaryColor = Color.fromARGB(255, 0, 31, 56);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+const Color kappColorDark = Colors.black;
+const Color kappColorLight = Colors.white;
+const Color kappPrimary = Colors.purple;
+const Color kappSecondary = Colors.orange;
+
+const Color kcardBackground = Colors.black12;
+const Color cardColor = Color.fromARGB(255, 75, 153, 218);
+
+// new
+const Color buttonColor = Color(0xff1967d2);
+const Color backColor = Color(0xfff5f7fc);
+const Color imagefield = Color(0xfffafbfc);
+const Color textColor2 = Color(0xff49509b);
+const Color appBarColor = Color(0xfff5f7fc);
+
+const primary = Color(0xFFFF7D31);
+const secondary = Color(0xFFFFA837);
+const darker = Color(0xFF3E4249);
+// const cardColor = Color(0xFFEDF0F3);
+const mainColor = Color(0xFF000000);
+const appBgColor = Color(0xFFFAFAFA);
+const shadowColor = Colors.black87;
+const textBoxColor = Color(0xFFe9e9e9);
+
+const bottomBarColor = Colors.black;
+const inActiveColor = Color(0xFF3E4249);
+
+const yellow = Color(0xFFffcb66);
+const green = Color(0xFFb2e1b5);
+const pink = Color(0xFFf5bde8);
+const purple = Color(0xFFd9bcff);
+const red = Color(0xFFFF968A);
+const orange = Color(0xFFFFC8A2);
+const sky = Color(0xFFABDEE6);
+const blue = Color(0xFF509BE4);
+
+const listColors = [
+  green,
+  primary,
+  pink,
+  red,
+  yellow,
+  orange,
+  sky,
+  blue,
+  purple,
+  yellow,
+];
+
+const Color spaceLight = Color(0xff2b3a67);
+const Color orangeWeb = Color(0xFFf59400);
+const Color white = Color(0xFFf5f5f5);
+const Color greyColor = Color(0xffaeaeae);
+const Color greyColor2 = Color(0xffE8E8E8);
+const Color lightGrey = Color(0xff928a8a);
+const Color burgundy = Color(0xFF880d1e);
+const Color indyBlue = Color(0xFF414361);
+const Color spaceCadet = Color(0xFF2a2d43);
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
